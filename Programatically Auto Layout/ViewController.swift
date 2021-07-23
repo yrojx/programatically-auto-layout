f948f3415a294956864ff612b4f8f8c58d5148d2
yrojx <yossansr06@gmail.com>
2021-07-23 10:34:02 +0700

    Initial Commit

---
+++ Programatically Auto Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Programatically Auto Layout
+//
+//  Created by Yossan Sandi Rahmadi on 23/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
